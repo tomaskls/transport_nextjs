dc95436b8058b9b16310fa406c99620beb76e464
tomaskls <tomaskls@hotmail.com>
2024-12-16 18:03:04 +0200

    beforo vercel

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
- 
+
+type SupportedLocale = typeof routing.locales[number];
+
 export default getRequestConfig(async ({requestLocale}) => {
-  // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
- 
-  // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as SupportedLocale)) {
     locale = routing.defaultLocale;
   }
- 
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,10 +2,7 @@ import {defineRouting} from 'next-intl/routing';
 import {createNavigation} from 'next-intl/navigation';
  
 export const routing = defineRouting({
-  // Visos palaikomos kalbos
   locales: ['lt', 'en', 'ru'],
- 
-  // Numatytoji kalba
   defaultLocale: 'lt'
 });
  
